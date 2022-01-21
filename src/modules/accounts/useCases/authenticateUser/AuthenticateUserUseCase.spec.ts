a47c7b7bd5d29c2e4304c280cb816d16c32e1307
rogerio-stubs <rostubs17@gmail.com>
2022-01-20 23:14:58 -0300

    Primeiros testes

---
+++ src/modules/accounts/useCases/authenticateUser/AuthenticateUserUseCase.spec.ts
@@ -0,0 +1,64 @@
+import { AppError } from "../../../../errors/AppError";
+import { ICreateUserDTO } from "../../dtos/ICreateUserDTO";
+import { UsersRepositoryInMemory } from "../../repositories/in-memory/UsersRepositoryInMemory";
+import { CreateUserUseCase } from "../createUser.ts/CreateUserUseCase";
+import { AuthenticateUserUseCase } from "./AuthenticateUserUseCase";
+
+let authenticateUseCase: AuthenticateUserUseCase;
+let usersRepositoryInMemory: UsersRepositoryInMemory;
+let createUserUseCase: CreateUserUseCase;
+
+describe("Authenticate User", () => {
+  beforeEach(() => {
+    usersRepositoryInMemory = new UsersRepositoryInMemory();
+    authenticateUseCase = new AuthenticateUserUseCase(usersRepositoryInMemory);
+    createUserUseCase = new CreateUserUseCase(usersRepositoryInMemory);
+  });
+
+  it("Should be able to authenticate an user", async () => {
+    const user:  ICreateUserDTO = {
+      driver_license: "0000",
+      email: "user@test.com",
+      password: "1234",
+      name: "User Test"
+    };
+
+    await createUserUseCase.execute(user);
+
+    const result = await authenticateUseCase.execute({
+      email: user.email,
+      password: user.password
+    });
+
+    expect(result).toHaveProperty("token");
+  });
+
+  it("should not be able to authenticate an nonexistent user", () => {
+    expect(async() => {
+      await authenticateUseCase.execute({
+        email: "false@email.com",
+        password: "1234"
+      });
+    }).rejects.toBeInstanceOf(AppError);
+  });
+
+  it("should not be able to authenticate with incorrect password", () => {
+    expect(async () => {
+      const user: ICreateUserDTO = {
+        driver_license: "0000",
+        email: "user@test.com",
+        password: "1234",
+        name: "User test Error"
+      };
+
+      await createUserUseCase.execute(user);
+
+      await authenticateUseCase.execute({
+        email: user.email,
+        password: "incorrectPassword",
+      });
+
+    }).rejects.toBeInstanceOf(AppError);
+  });
+
+});
--- src/modules/cars/useCases/createCategory/CreateCategoryUseCase.spec.ts
+++ src/modules/cars/useCases/createCategory/CreateCategoryUseCase.spec.ts
@@ -1,5 +1,50 @@
+import { AppError } from "../../../../errors/AppError";
+import { CategoriesRespositoryInMemory } from "../../repositories/in-memory/CategoriesRepositoryInMemory";
+import { CreateCategoryUseCase } from "./CreateCategoryUseCase";
+
+let createCategoryUseCase: CreateCategoryUseCase;
+let categoriesRespositoryInMemory: CategoriesRespositoryInMemory;
+
 describe("Create Category", () => {
-  it("should be able to create a new category", () => {
-    // const createCategory = new CreateCategoryUseCase();
+
+  beforeEach(() => {
+    categoriesRespositoryInMemory = new CategoriesRespositoryInMemory();
+    createCategoryUseCase = new CreateCategoryUseCase(categoriesRespositoryInMemory);
+  });
+
+  it("should be able to create a new category", async () => {
+    const category = {
+      name: "Category Test",
+      description: "Category description Test"
+    };
+
+    await createCategoryUseCase.execute({
+      name: category.name,
+      description: category.description,
+    });
+
+    const categoryCreated = await categoriesRespositoryInMemory.findByName(category.name);
+
+    expect(categoryCreated).toHaveProperty("id");
+  });
+
+
+  it("should be able to create a new category with name exists", async () => {
+    expect(async () => {
+      const category = {
+        name: "Category Test",
+        description: "Category description Test"
+      };
+  
+      await createCategoryUseCase.execute({
+        name: category.name,
+        description: category.description,
+      });
+  
+      await createCategoryUseCase.execute({
+        name: category.name,
+        description: category.description,
+      });
+    }).rejects.toBeInstanceOf(AppError);
   });
 });
